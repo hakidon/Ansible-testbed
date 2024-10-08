fb7b3ac7ce92b6af7b13f44f36b2fa645bfd93b6
hakidon <mohdanihakim@gmail.com>
2024-10-08 16:54:23 +0800

    Update backend nodemon

--- server.js
+++ server.js
@@ -1,10 +1,27 @@
 const express = require('express');
 const { exec } = require('child_process');
+const { Client } = require('pg');
 
 const app = express();
 const PORT = 5000;
 
 app.use(express.json());
+
+const client = new Client({
+  user: 'postgres',
+  host: 'db',
+  database: 'ansible',
+  password: 'test123',
+  port: 5432, // default PostgreSQL port
+});
+
+client.connect(err => {
+  if (err) {
+    console.error('Connection error', err.stack);
+  } else {
+    console.log('Connected to PostgreSQL');
+  }
+});
 
 app.get('/', (req, res) => {
   res.json({ message: "Backend is running!", code: 0 });
@@ -30,6 +47,15 @@ app.get('/ansible-pb-info', (req, res) => {
     }
     res.json({ stdout, code: 0 });
   });
+});
+
+app.get('/db-check', async (req, res) => {
+  try {
+    const result = await client.query('SELECT NOW()');
+    res.json({ message: 'Connected to PostgreSQL', time: result.rows.now, code: 0 });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
 });
 
 app.listen(PORT, () => {
