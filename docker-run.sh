65f6aa67762bfe8d5d07d617867de1a2546e8422
hakidon <mohdanihakim@gmail.com>
2024-10-08 15:02:14 +0800

    Update

---
+++ docker-run.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Create a directory named Dockerfiles
+mkdir -p Dockerfiles
+
+# Download files into the Dockerfiles directory
+curl -o Dockerfiles/Dockerfile-db https://raw.githubusercontent.com/hakidon/Ansible-testbed/refs/heads/main/Dockerfiles/Dockerfile-db.yml
+curl -o Dockerfiles/Dockerfile-be https://raw.githubusercontent.com/hakidon/Ansible-testbed/refs/heads/main/Dockerfiles/Dockerfile-be.yml
+curl -o Dockerfiles/Dockerfile-fe https://raw.githubusercontent.com/hakidon/Ansible-testbed/refs/heads/main/Dockerfiles/Dockerfile-fe.yml
+curl -o Dockerfiles/docker-compose.yml https://raw.githubusercontent.com/hakidon/Ansible-testbed/refs/heads/main/Dockerfiles/docker-compose.yml
+
+# Start the Docker Compose services
+sudo docker compose -f Dockerfiles/docker-compose.yml up -d
