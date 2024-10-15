ca5c25e47e2407ddf411ced3c260148567dc6db3
hakidon <mohdanihakim@gmail.com>
2024-10-15 12:42:40 +0800

    Update script init

--- docker-run.sh
+++ docker-run.sh
@@ -9,11 +9,14 @@ curl -o Dockerfiles/Dockerfile-be https://raw.githubusercontent.com/hakidon/Ansi
 curl -o Dockerfiles/Dockerfile-fe https://raw.githubusercontent.com/hakidon/Ansible-testbed/refs/heads/main/Dockerfiles/Dockerfile-fe
 curl -o Dockerfiles/docker-compose.yml https://raw.githubusercontent.com/hakidon/Ansible-testbed/refs/heads/main/Dockerfiles/docker-compose.yml
 
-curl -o Dockerfiles/be/server.js https://raw.githubusercontent.com/hakidon/Ansible-testbed/refs/heads/main/be/server.js
-curl -o Dockerfiles/fe/download-repo.sh https://raw.githubusercontent.com/hakidon/Ansible-testbed/refs/heads/main/scripts/download-repo.sh
+mkdir -p Dockerfiles/be
+mkdir -p Dockerfiles/fe
 
-chmod +x Dockerfiles/fe/download-repo.sh
-.download-repo.sh https://api.github.com/repos/hakidon/Ansible-testbed/contents/fe/src Dockerfiles/fe
+curl -o Dockerfiles/be/server.js https://raw.githubusercontent.com/hakidon/Ansible-testbed/refs/heads/main/be/server.js
+curl -o download-repo.sh https://raw.githubusercontent.com/hakidon/Ansible-testbed/refs/heads/main/scripts/download-repo.sh
+
+chmod +x download-repo.sh
+./download-repo.sh https://api.github.com/repos/hakidon/Ansible-testbed/contents/fe/src Dockerfiles/fe
 
 
 # Start the Docker Compose services
