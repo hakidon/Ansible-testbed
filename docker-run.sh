7a334910e8e5374ad41ab417833ebaf58aa6a51f
hakidon <mohdanihakim@gmail.com>
2024-10-15 12:31:02 +0800

    Update scripts

--- docker-run.sh
+++ docker-run.sh
@@ -9,5 +9,12 @@ curl -o Dockerfiles/Dockerfile-be https://raw.githubusercontent.com/hakidon/Ansi
 curl -o Dockerfiles/Dockerfile-fe https://raw.githubusercontent.com/hakidon/Ansible-testbed/refs/heads/main/Dockerfiles/Dockerfile-fe
 curl -o Dockerfiles/docker-compose.yml https://raw.githubusercontent.com/hakidon/Ansible-testbed/refs/heads/main/Dockerfiles/docker-compose.yml
 
+curl -o Dockerfiles/be/server.js https://raw.githubusercontent.com/hakidon/Ansible-testbed/refs/heads/main/be/server.js
+curl -o Dockerfiles/fe/download-repo.sh https://raw.githubusercontent.com/hakidon/Ansible-testbed/refs/heads/main/scripts/download-repo.sh
+
+chmod +x Dockerfiles/fe/download-repo.sh
+./Dockerfiles/fe/download-repo.sh https://api.github.com/repos/hakidon/Ansible-testbed/contents/fe/src
+
+
 # Start the Docker Compose services
 # sudo docker compose -f Dockerfiles/docker-compose.yml up -d
---
+++ scripts/download-repo.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+# Function to download files
+download_files() {
+  local dir=$1
+  local json=$2
+  local urls=$(echo $json | grep -oP '"download_url": "\K(.*?)(?=")')
+  local dir_urls=$(echo $json | grep -oP '"self": "\K(.*?)(?=")')
+  local types=$(echo $json | grep -oP '"type": "\K(.*?)(?=")')
+  local names=$(echo $json | grep -oP '"name": "\K(.*?)(?=")')
+
+  local i=0
+  while read -r type; do
+    url=$(echo $urls | cut -d ' ' -f $((i + 1)))
+    name=$(echo $names | cut -d ' ' -f $((i + 1)))
+    dir_url=$(echo $dir_urls | cut -d ' ' -f $((i + 1)))
+
+    if [ "$type" == "dir" ]; then
+      mkdir -p "$dir/$name"
+      json=$(curl -s "$dir_url")
+      download_files "$dir/$name" "$json"
+    else
+      curl -o "$dir/$name" "$url"
+    fi
+    i=$((i + 1))
+  done <<< "$types"
+}
+
+# Check if URL is provided
+if [ -z "$1" ]; then
+  echo "Usage: $0 <url>"
+  exit 1
+fi
+
+# Fetch the JSON response
+json=$(curl -s "$1")
+
+# Start downloading files
+download_files "." "$json"
