3c0d6ccd99282090b4f17edd9f3aeaaf330697d5
hakidon <mohdanihakim@gmail.com>
2024-10-16 17:04:47 +0800

    Update run sh

--- run.sh
+++ run.sh
@@ -1,8 +1,25 @@
 #!/bin/bash
+
+# Update package list and install necessary packages
 sudo apt update
 sudo apt install git curl -y
-sudo snap install docker
 
+# Function to show a loading progress bar
+show_progress() {
+    echo -n "Installing Docker"
+    while ps aux | grep -q "[s]nap install docker"; do
+        echo -n "."
+        sleep 1
+    done
+    echo " Done!"
+}
+
+# Install Docker with a progress indicator
+sudo snap install docker &
+show_progress
+
+# Clone the repository
 git clone https://github.com/hakidon/Ansible-testbed.git 
 
+# Start Docker containers
 sudo docker compose -f Ansible-testbed/docker-compose.yml up -d
