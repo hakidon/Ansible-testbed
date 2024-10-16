350f5a747c30bb40600ada959ff960c0b00633d7
hakidon <mohdanihakim@gmail.com>
2024-10-16 10:12:21 +0800

    Update directory

--- docker-run.sh
+++ docker-run.sh
@@ -4,9 +4,9 @@
 mkdir -p Dockerfiles
 
 # Download files into the Dockerfiles directory
-curl -o Dockerfiles/Dockerfile-db https://raw.githubusercontent.com/hakidon/Ansible-testbed/refs/heads/main/Dockerfiles/Dockerfile-db
-curl -o Dockerfiles/Dockerfile-be https://raw.githubusercontent.com/hakidon/Ansible-testbed/refs/heads/main/Dockerfiles/Dockerfile-be
-curl -o Dockerfiles/Dockerfile-fe https://raw.githubusercontent.com/hakidon/Ansible-testbed/refs/heads/main/Dockerfiles/Dockerfile-fe
+# curl -o Dockerfiles/Dockerfile-db https://raw.githubusercontent.com/hakidon/Ansible-testbed/refs/heads/main/Dockerfiles/Dockerfile-db
+# curl -o Dockerfiles/Dockerfile-be https://raw.githubusercontent.com/hakidon/Ansible-testbed/refs/heads/main/Dockerfiles/Dockerfile-be
+# curl -o Dockerfiles/Dockerfile-fe https://raw.githubusercontent.com/hakidon/Ansible-testbed/refs/heads/main/Dockerfiles/Dockerfile-fe
 curl -o Dockerfiles/docker-compose.yml https://raw.githubusercontent.com/hakidon/Ansible-testbed/refs/heads/main/Dockerfiles/docker-compose.yml
 
 mkdir -p Dockerfiles/be
@@ -20,4 +20,4 @@ chmod +x download-repo.sh
 
 
 # Start the Docker Compose services
-sudo docker compose -f Dockerfiles/docker-compose.yml up -d
+#sudo docker compose -f Dockerfiles/docker-compose.yml up -d
